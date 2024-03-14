8c8ce5d2ba2a1fcdf7ecf383b4fe0126d0f01318
orymikoto <rosbergroll99@gmail.com>
2024-03-15 04:12:41 +0700

    secure api route

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 "use server";
 
 import axios from "axios";
-import Image from "next/image";
 
 export default async function Home() {
-  const response = await axios.get(
-    "https://next-probation-22.sepakat.cloud/api/probation/properties"
-  );
+  const response = await axios.get(process.env.Source_API + "properties");
 
-  console.log(response.data.data);
+  // console.log(response.data.data);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
